b75fee9cb2c420627e3748e5ddfc937eea45968a
viktorbo <borisoof13@yandex.ru>
2022-03-21 13:21:41 +0300

    Added general files

---
+++ settings.py
@@ -0,0 +1,22 @@
+WINDOW_TITLE = 'PHELDA PROJECT'
+
+
+WIDTH = 800
+HEIGHT = 600
+
+FPS = 60
+
+WORLD_MAP = [
+    list('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+    list('x                                        x'),
+    list('x                                        x'),
+    list('x                                        x'),
+    list('x               xx  xx                   x'),
+    list('x               xxxxxx                   x'),
+    list('x                xxxx                    x'),
+    list('x                 xx                     x'),
+    list('x                                        x'),
+    list('x                                        x'),
+    list('x                                        x'),
+    list('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+]
